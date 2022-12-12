8780e441294e9237841f5706759b1189d8e73847
Victor <manuelher1619@gmail.com>
2022-12-11 22:56:14 -0600

    Tercer ejercicio

---
+++ ej3/js/main.js
@@ -0,0 +1,31 @@
+/*  Blockquote
+**Casos de prueba**:
+
+_Caso 1_: Resultado esperado: El número 5 se encuentra más de una vez
+[1, 22, 5, 17, 10, 5, 40, 5]
+
+_Caso 2_: Resultado esperado: El número 7 se encuentra más de una vez
+[7, 41, 5, 7, 10, 13, 2]
+
+_Caso 3_: Resultado esperado: No hay números repetidos
+[1, 22, 5, 14, 24, 31, 27, 15, 105]
+*/
+// CASO 1
+let arreglo = [1, 22, 5, 17, 10, 5, 40, 5];
+ let n1 = 5;
+//CASO 2
+ /*let arreglo = [7, 41, 5, 7, 10, 13, 2];
+let n1 = 7;*/
+//CASO 3
+/*let arreglo = [1, 22, 5, 14, 24, 31, 27, 15, 105];
+let n1 = null;*/
+
+
+    for (let arreglo = 0; index < arreglo.length; index++) {
+        const element = arreglo[index];
+        
+    }
+
+console.log(arreglo);
+
+ 
