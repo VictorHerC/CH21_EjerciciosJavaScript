96ccad503a694f76ee32cb692dbe559a1999d447
Victor <manuelher1619@gmail.com>
2022-12-12 01:03:31 -0600

    Ejercicio 5 y 6

---
+++ ej5/js/main.js
@@ -0,0 +1,55 @@
+/* # Ejercicios en Javascript Segunda Parte
+### Implementación de los ejercicios con Javascript
+
+1. Solicitar 3 números (entre el 1 y el 100)  y definir cual es el mayor
+2. Solicitar 3 números (entre el 1 y el 100)  y definir el menor de tres números
+3. Adivinar un número entre el 1 y el 100 en el menor número de pasos posibles
+4. Solicitar un entero (entre el 100 y el 200) y determinar si es múltiplo de 3
+5. Elaborar un algoritmo para leer 3 números y determinar sí uno es la suma de los otros dos
+6. Elabora un algoritmo para leer un número y determinar si es par o impar
+*/ 
+
+// CASO1
+/*let n1,n2,n3,t,mayor;
+
+	n1 = parseInt(prompt("Ingresa numero 1"));
+	n2 = parseInt(prompt("Ingresa numero 2"));
+	n3 = parseInt(prompt("Ingresa numero 3"));
+
+	if (n1 > n2) {
+		t = n1;
+	}else{
+		t = n2;
+	}
+
+	if (t > n3) {
+		mayor = t;
+	}else{
+		mayor = n3;
+	}
+
+	document.write("El mayor es "+mayor);*/
+    
+    //CASO 2
+    /*let n1,n2,n3,t,menor;
+
+	n1 = parseInt(prompt("Ingresa numero 1"));
+	n2 = parseInt(prompt("Ingresa numero 2"));
+	n3 = parseInt(prompt("Ingresa numero 3"));
+
+	if (n1 < n2) {
+		t = n1;
+	}else{
+		t = n2;
+	}
+
+	if (t < n3) {
+		menor = t;
+	}else{
+		menor = n3;
+	}
+
+	document.write("El menor es "+menor);*/
+    let arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+
+	
